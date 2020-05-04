4102c9077bfc2d3095ae5b9362bf034981204bab
Martynova Alexandra <a.v.martynova01@gmail.com>
2020-05-04 18:41:50 +0300

    added ExceptionListener

---
+++ src/EventListener/HTTPExceptionListener.php
@@ -0,0 +1,37 @@
+<?php
+declare(strict_types=1);
+
+namespace App\EventListener;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
+
+class HTTPExceptionListener
+{
+    public function onKernelException(ExceptionEvent $event)
+    {
+        $exception = $event->getThrowable();
+        $message = sprintf(
+            'My Error says: %s with code: %s',
+            $exception->getMessage(),
+            $exception->getCode()
+        );
+
+        // Customize your response object to display the exception details
+        $response = new JsonResponse();
+        $response->setJson(json_encode(['error' => $message]));
+
+        // HttpExceptionInterface is a special type of exception that
+        // holds status code and header details
+        if ($exception instanceof HttpExceptionInterface) {
+            $response->setStatusCode($exception->getStatusCode());
+            $response->headers->replace($exception->getHeaders());
+        } else {
+            $response->setStatusCode(Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+
+        $event->setResponse($response);
+    }
+}
